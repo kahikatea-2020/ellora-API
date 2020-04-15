bccf64f25fcabb3ed839695ec2059ad5cdb4fd4d
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-15 16:59:00 +1200

    Add separate routes.js file

--- server/server.js
+++ server/server.js
@@ -6,4 +6,6 @@ const server = express()
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
+server.use('/', require('./routes/routes'))
+
 module.exports = server
