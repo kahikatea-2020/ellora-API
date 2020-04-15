bd698c641275d1b2c229e66167835bcd53fcee10
Ellora Virtue <ellora.virtue@hotmail.com>
2020-04-15 16:34:56 +1200

    Add test data to this.state to verify everything renders

--- client/components/Cocktails.jsx
+++ client/components/Cocktails.jsx
@@ -8,15 +8,20 @@ class Cocktails extends React.Component {
     super()
 
     this.state = {
-      name: '',
-      instructions: '',
-      image: ''
+      name: 'Margarita',
+      image: 'https://www.thecocktaildb.com/images/media/drink/wpxpvu1439905379.jpg',
+      instructions: 'Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.'
     }
   }
 
   render () {
     return (
-      <h2>Classic cocktails</h2>
+      <>
+        <h2>Classic cocktails</h2>
+        <div>{this.state.name}</div>
+        <img src={this.state.image} alt={`image of a ${this.state.name}`} />
+        <div>Instructions: {this.state.instructions}</div>
+      </>
     )
   }
 }
